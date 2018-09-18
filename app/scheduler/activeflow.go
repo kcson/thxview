37c3db4af5334f57758f82fe63110f3a56374c34
kcson <kcson@fasoo.comm>
2018-09-18 22:54:08 +0900

    로그 conf 설정

--- app/scheduler/activeflow.go
+++ app/scheduler/activeflow.go
@@ -3,17 +3,18 @@ package scheduler
 import (
 	"fmt"
 	"github.com/jasonlvhit/gocron"
+	"github.com/revel/revel"
 )
 
-func ActiveFlowJob () {
+func ActiveFlowJob() {
 	fmt.Println("call ActiveFlowJob")
 
 	go func() {
 		gocron.Every(10).Seconds().Do(runJob)
-		<- gocron.Start()
+		<-gocron.Start()
 	}()
 }
 
 func runJob() {
-	fmt.Println("call runJob")
+	revel.AppLog.Debug("call runJob")
 }
