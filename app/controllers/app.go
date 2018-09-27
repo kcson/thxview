19d7682b6fadbae57692f6bd6ae806d5a6dc4f72
yongmin <yongmin.jung@thxcloud.com>
2018-09-27 19:40:49 +0900

    user login 추가(admin1, user 계정만 적용) : db 작업 후 사용 가능

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
-	"github.com/revel/revel"
+	"context"
+	"database/sql"
 	"fmt"
+	"github.com/revel/revel"
+	"log"
 	"strings"
 )
 
@@ -24,13 +27,101 @@ func (c App) Login() revel.Result {
 	return c.Render()
 }
 
+func Conn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "express2"
+	dbPass := "imsi00"
+	dbName := "tcp(127.0.0.1:3306)/exseoul"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+var ctx = context.Background()
+
 func (c App) CreateSession() revel.Result {
+	//revel.INFO.Println("CreateSession.logincheck ========")
 	var reqParam = make(map[string]interface{})
 	result := make(map[string]interface{})
 
 	c.Params.BindJSON(&reqParam)
 
 	username := reqParam["username"]
+	password := reqParam["password"]
+	//revel.INFO.Println("username==================", username)
+	//revel.INFO.Println("password==================", password)
+
+	//Login Check start
+	if username == "admin1" || username == "user" {
+		if username != nil && password != nil {
+
+			//DB := Conn()
+			DB := Login.dbConn()
+			revel.INFO.Println("CreateSession.logincheck : DB Connected......")
+
+			rows, err := DB.QueryContext(ctx, "SELECT id,password,role FROM thx_employee WHERE id=?", username)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer rows.Close()
+
+			type userInfo struct {
+				id       string
+				password string
+				role     int
+			}
+
+			var items []userInfo
+			for rows.Next() {
+				item := userInfo{}
+
+				err := rows.Scan(
+					&item.id,
+					&item.password,
+					&item.role,
+				)
+				CheckErr(err)
+
+				revel.INFO.Println("Item id :", item.id)
+				revel.INFO.Println("Item password :", item.password)
+				revel.INFO.Println("Item role :", item.role)
+
+				items = append(items, item)
+			}
+			//revel.INFO.Println("Item num :", len(items))
+			//revel.INFO.Println("items[0].password:", items[0].password)
+
+			if err != nil {
+				revel.INFO.Println("DB Error", err)
+			}
+
+			if len(items) == 0 {
+				revel.INFO.Println("CreateSession.logincheck : There is no user.....")
+				return c.Redirect(App.Index)
+			}
+
+			if len(items) > 0 && password == items[0].password {
+				revel.INFO.Println("CreateSession.logincheck : Password matching.....")
+				result["auth"] = "success"
+				//return c.Redirect(App.Dashboard)
+			} else {
+				revel.INFO.Println("CreateSession.logincheck : Password is wrong.....")
+				result["auth"] = "fail"
+				c.Message("Password is wrong!!!")
+				return c.Redirect(App.Index)
+			}
+			//return c.Redirect(App.Dashboard)
+		} else {
+			revel.INFO.Println("CreateSession.logincheck : Missing value.....")
+			c.Validation.Required(username).Message("Your name is required!")
+			result["auth"] = "fail"
+			return c.Redirect(App.Index)
+		}
+	}
+	//Login Check end
+
 	if username != nil {
 		c.Session["authKey"] = "authKey"
 		c.Session["userName"] = username.(string)
@@ -44,7 +135,6 @@ func (c App) CreateSession() revel.Result {
 	} else {
 		result["auth"] = "fail"
 	}
-
 	return c.RenderJSON(result)
 }
 
